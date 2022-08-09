5d799ace6407c715dedeb647653523a7740df5d1
justinshin6 <justinshin@brown.edu>
2022-08-09 00:53:15 -0400

    add login feature

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 // bring in functions from the userController
-const {registerUser, loginUser} = require('../controller/userController')
+const {registerUser, loginUser, getMe} = require('../controller/userController')
 
 router.post('/', registerUser)
-
 router.post('/login', loginUser)
+router.get('/me', getMe)
+
 module.exports = router
